c189cec9ee279eda1a68938ec28db27027dc5a0b
Wilhelm Gustavsson <wilhelm.gustavsson8992@gmail.com>
2023-03-21 07:11:53 +0100

    local images now used

--- client/src/work.js
+++ client/src/work.js
@@ -29,8 +29,8 @@ function Work()
         <WorkList></WorkList>
         <section id = "badges">
 
-            <a href="https://github.com/wgustavsson1"><img src ="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg" class="badge"/></a>
-            <a href="https://www.linkedin.com/in/wilhelm-gustavsson/"><img src ="https://cdn-icons-png.flaticon.com/512/174/174857.png" class="badge"/></a>
+            <a href="https://github.com/wgustavsson1"><img src={require('./images/github.png')} class="badge"/></a>
+            <a href="https://www.linkedin.com/in/wilhelm-gustavsson/"><img src={require('./images/linkedin.png')} class="badge"/></a>
         </section>
     </div>
     )

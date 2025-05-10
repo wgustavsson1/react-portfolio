778d54a8a1fa6533a402c2afa3204f154965d4bd
Wilhelm Gustavsson <wilhelm.gustavsson8992@gmail.com>
2025-05-10 18:54:23 +0200

    fix

--- client/src/contact.js
+++ client/src/contact.js
@@ -71,7 +71,7 @@ const ContactForm = () => {
           className="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
           type="submit"
         >
-          Send!
+          Skicka!
         </button>
       </div>
     </form>
